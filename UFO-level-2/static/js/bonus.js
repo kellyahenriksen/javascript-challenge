5fb6e88dc9322e37dd7d3da819ccc0e9957fc316
Kelly <kellyahenriksen@gmail.com>
2021-03-10 12:28:34 -0500

    finished part 1, working on bonus

--- UFO-level-2/static/js/bonus.js
+++ UFO-level-2/static/js/bonus.js
@@ -29,9 +29,21 @@ function runEnter() {
 
   // Prevent the page from refreshing
   d3.event.preventDefault();
-  
+
+/////////////////////////////
+  // use "this" function to grab all ids
+//   var inputid = 
+
+  d3.selectAll("input").on("click", function() {
+    // you can select the element just like any other selection
+    var listItem = d3.select(this);
+    var inputid = listItem.id();
+    console.log(inputid);
+});
+
+
   // Select the input element and get the raw HTML node
-  var inputElement = d3.select("#datetime");
+  var inputElement = d3.select(inputid);
 
   // Get the value property of the input element
   var inputValue = inputElement.property("value");
@@ -39,7 +51,9 @@ function runEnter() {
   console.log(inputValue);
   console.log(tableData);
 
-  var filteredData = tableData.filter(UFOSighting => UFOSighting.datetime === inputValue);
+  var filteredData = tableData.filter(UFOSighting => UFOSighting.inputid === inputValue);
+
+  ////////////////////////////
 
   console.log(filteredData);
 
